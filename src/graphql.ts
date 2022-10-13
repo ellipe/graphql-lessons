0599f4384590a802d3893103040c573741d0ebd9
Luis Bertel <ellipe@gmail.com>
2022-10-13 15:03:24 -0500

    initial commit

---
+++ src/graphql.ts
@@ -0,0 +1,16 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export interface IQuery {
+    __typename?: 'IQuery';
+    hello(): string | Promise<string>;
+}
+
+type Nullable<T> = T | null;
---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  @Query((returns) => LessonType)
+  lesson() {
+    return {
+      id: 'ssadas',
+      name: 'Hardcoded',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
